a53c011a31441e98026d85766179f0524c06ddbc
Hector Nava <hector.r.nava44@gmail.com>
2022-01-31 18:18:57 -0700

    Added fs and uuid vimports

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const notesData = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
