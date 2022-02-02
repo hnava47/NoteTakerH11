54b9ab5de8b7ee26e527fdb6156d162f9c9befc6
Hector Nava <hector.r.nava44@gmail.com>
2022-02-02 07:17:27 -0700

    Removed slash

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.delete('/api/notes/:id', (req, res) => {
     }
 });
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
